cf93667142f053a3933d81d7fc527bbcba139aa7
mandzokralju <rober.jan22@gmail.com>
2017-11-27 17:10:20 +0100

    dodana animacija

--- app/src/main/java/com/example/jan/gps_koordinate/PopUp.java
+++ app/src/main/java/com/example/jan/gps_koordinate/PopUp.java
@@ -3,6 +3,8 @@ package com.example.jan.gps_koordinate;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
@@ -10,6 +12,7 @@ import android.widget.VideoView;
 public class PopUp extends AppCompatActivity {
 
     private VideoView video;
+    private Button nazaj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +23,18 @@ public class PopUp extends AppCompatActivity {
         int id = b.getInt("id");
 
         video = (VideoView)findViewById(R.id.videoView);
+        nazaj = (Button)findViewById(R.id.btnPopBack);
+        nazaj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopUp.super.onBackPressed();
+            }
+        });
 
         //PopUp prikaže navodila za uporabo AED
         if(id == 1){
-            video.setVideoPath("android.resource://"+ getPackageName()+"/" + R.raw.aed);
+            video.setVideoPath("android.resource://"+ getPackageName()+"/" + R.raw.test2);
+            video.setMediaController(new MediaController(this));
             video.start();
         }
     }
