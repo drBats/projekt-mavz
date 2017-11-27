cc0aa71fa6a4b462c912d07d802a55f03e144e4d
mandzokralju <rober.jan22@gmail.com>
2017-11-27 15:33:17 +0100

    GitIgnore update

--- openCVLibrary320/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary320/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\Klemen\\Documents\\Repositories\\projekt-mavz\\openCVLibrary320\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\Kizo\\Desktop\\Project\\projekt-mavz\\openCVLibrary320\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
