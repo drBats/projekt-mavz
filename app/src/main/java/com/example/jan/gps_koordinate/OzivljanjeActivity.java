83d644679365c4efcc6fc66dfd6c785a2b7a91b6
mandzokralju <rober.jan22@gmail.com>
2017-12-02 19:46:33 +0100

    Osnovno delovanje simulacije oživljanja

--- app/src/main/java/com/example/jan/gps_koordinate/OzivljanjeActivity.java
+++ app/src/main/java/com/example/jan/gps_koordinate/OzivljanjeActivity.java
@@ -2,12 +2,47 @@ package com.example.jan.gps_koordinate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class OzivljanjeActivity extends AppCompatActivity {
+
+    Button btnOzivljanje;
+    TextView text;
+    double time_presssed,time_now, end_time, time_before;
+    double time_first=0, times_clicked=0;
+    double BPM;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ozivljanje);
+
+        btnOzivljanje = (Button)findViewById(R.id.button_ress);
+        text = (TextView)findViewById(R.id.textView2);
+
+        btnOzivljanje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(time_first == 0){
+                    //Začetni čas
+                    time_first = System.currentTimeMillis();
+                    time_before = System.currentTimeMillis();
+                    times_clicked++;
+                }
+                else{
+                    time_now=System.currentTimeMillis();
+                    times_clicked++;
+                    BPM = 60 / ((time_now - time_before)/1000);
+                    text.setText(String.valueOf((int)BPM));
+                    time_before=time_now;
+                }
+            }
+        });
     }
 }
